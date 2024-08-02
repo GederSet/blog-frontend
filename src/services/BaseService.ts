062752799b69fad0547658df96a7b1a5ee46de8d
Grisha <grischastulov10@gmail.com>
2024-08-03 01:29:09 +0300

    change env file

--- src/services/BaseService.ts
+++ src/services/BaseService.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { parseCookies } from 'nookies'
 
-const baseUrl = process.env.VITE_API_URL
+const baseUrl = import.meta.env.VITE_API_URL
 
 export const baseApi = createApi({
   tagTypes: ['User', 'Post', 'File', 'Comment'],
